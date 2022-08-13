3e2d625fd3aa281ce1e48c2aaa9369bd984c7de5
Konstantin <tofilyk@mail.ru>
2022-08-13 12:05:56 +0300

    Add Classes

--- Sandbox/src/main/java/ru/stqa/oft/sandbox/MyFirstProject.java
+++ Sandbox/src/main/java/ru/stqa/oft/sandbox/MyFirstProject.java
@@ -5,18 +5,23 @@ public class MyFirstProject {
 
     public static void main(String[] args) {
         hellow("Test");
-   double l=5;
-     System.out.println("Площадь квадрата со стороной "+l+"="+area(l));
+        Square s = new Square(5);
 
-    double a=4;
-    double b =6;
-System.out.println("Площадь прямоугольник со сторонами  "+a+" и "+b+"="+ area(a,b)); }
-
-        public static void hellow(String samebode){
-                   System.out.println("Hello "+samebode);
-        }
-
-        public static double area(double len){return len*len;}
-
-    public static double area(double a,double b){return a*b;}
+        System.out.println("Площадь квадрата со стороной " + s.l + "=" + area(s));
+        Rectangle r = new Rectangle(4,6);
+        
+        System.out.println("Площадь прямоугольник со сторонами  " + r.a + " и " + r.b + "=" + area(r));
     }
+
+    public static void hellow(String samebode) {
+        System.out.println("Hello " + samebode);
+    }
+
+    public static double area(Square s) {
+        return s.l * s.l;
+    }
+
+    public static double area(Rectangle r) {
+        return r.a * r.b;
+    }
+}
