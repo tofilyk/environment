a42a189cab138a96f32b62d33c2d6d77f888d44c
Konstantin <tofilyk@mail.ru>
2022-08-13 12:13:29 +0300

    FuncInClass

--- Sandbox/src/main/java/ru/stqa/oft/sandbox/MyFirstProject.java
+++ Sandbox/src/main/java/ru/stqa/oft/sandbox/MyFirstProject.java
@@ -7,21 +7,15 @@ public class MyFirstProject {
         hellow("Test");
         Square s = new Square(5);
 
-        System.out.println("Площадь квадрата со стороной " + s.l + "=" + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + "=" + s.area());
         Rectangle r = new Rectangle(4,6);
-        
-        System.out.println("Площадь прямоугольник со сторонами  " + r.a + " и " + r.b + "=" + area(r));
+
+        System.out.println("Площадь прямоугольник со сторонами  " + r.a + " и " + r.b + "=" + r.area());
     }
 
     public static void hellow(String samebode) {
         System.out.println("Hello " + samebode);
     }
 
-    public static double area(Square s) {
-        return s.l * s.l;
-    }
 
-    public static double area(Rectangle r) {
-        return r.a * r.b;
-    }
 }
