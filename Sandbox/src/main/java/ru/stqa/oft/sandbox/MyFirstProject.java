225369b976ef008ff7e19564c3f8d287d65a4ecb
Konstantin <tofilyk@mail.ru>
2022-08-13 11:34:39 +0300

    Add functions area

--- Sandbox/src/main/java/ru/stqa/oft/sandbox/MyFirstProject.java
+++ Sandbox/src/main/java/ru/stqa/oft/sandbox/MyFirstProject.java
@@ -3,8 +3,20 @@ package ru.stqa.oft.sandbox;
 public class MyFirstProject {
 
 
+    public static void main(String[] args) {
+        hellow("Test");
+   double l=5;
+     System.out.println("Площадь квадрата со стороной "+l+"="+area(l));
 
-public static void main(String[] args){
-System.out.println("Hello First Text");
-}
-}
+    double a=4;
+    double b =6;
+System.out.println("Площадь прямоугольник со сторонами  "+a+" и "+b+"="+ area(a,b)); }
+
+        public static void hellow(String samebode){
+                   System.out.println("Hello "+samebode);
+        }
+
+        public static double area(double len){return len*len;}
+
+    public static double area(double a,double b){return a*b;}
+    }
