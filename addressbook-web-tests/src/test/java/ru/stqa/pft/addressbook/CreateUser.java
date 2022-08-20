da602ef18aca7fcbdbce0685fbf123e1002fb8c2
Konstantin <tofilyk@mail.ru>
2022-08-20 15:54:01 +0300

    Lesson 4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateUser.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateUser.java
@@ -11,64 +11,94 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class CreateUser {
-  ChromeDriver wd;
+    ChromeDriver wd;
 
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-
-    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-  }
-
-  @Test
-  public void testCreateUser() throws Exception {
-    wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.linkText("add new")).click();
-    wd.get("http://localhost/addressbook/edit.php");
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Konstantin");
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("Ivanov");
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("kosta");
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("Elisoft");
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("91122233555");
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    wd.findElement(By.linkText("home page")).click();
-    wd.get("http://localhost/addressbook/index.php");
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        wd.get("http://localhost/addressbook/group.php");
+        login("admin", "secret");
     }
-  }
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    private void login(String username, String pass) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(pass);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
-  }
+
+    @Test
+    public void testCreateUser() throws Exception {
+        goToPage();
+        createUser();
+        fillUser(new UserData("Ivan", "Ivanov", "vanya", "Elisoft", "91122233555"));
+        submitsUser();
+        returnHome();
+
+    }
+
+    private void goToPage() {
+        wd.get("http://localhost/addressbook/group.php");
+    }
+
+    private void returnHome() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    private void submitsUser() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void createUser() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    private void fillUser(UserData userData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(userData.getFirstname());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(userData.getMiddlename());
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(userData.getNickname());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(userData.getCompany());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(userData.getMobile());
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        wd.quit();
+
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 
 
-  }
+}
 
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class UserData {
+    private final String firstname;
+    private final String middlename;
+    private final String nickname;
+    private final String company;
+    private final String mobile;
+
+    public UserData(String firstname, String middlename, String nickname, String company, String mobile) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.nickname = nickname;
+        this.company = company;
+        this.mobile = mobile;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+}
