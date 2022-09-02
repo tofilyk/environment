477020c5f0f7c8aee70722a85b09c01855656400
Konstantin <tofilyk@mail.ru>
2022-09-02 20:19:41 +0300

    collections and cycles 4.6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -75,12 +75,12 @@ public class GroupHelper extends HelperBase {
 
     public List<GroupData> getGroupList() {
         List<GroupData> groups = new ArrayList<GroupData>();
-        List<WebElement> elements =wd.findElements(By.cssSelector("span.group"));
-                for( WebElement element: elements){
-                    String name= element.getText();
-                    GroupData group=new GroupData(name,null,null);
-                    groups.add(group);
-                }
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements) {
+            String name = element.getText();
+            GroupData group = new GroupData(name, null, null);
+            groups.add(group);
+        }
         return groups;
     }
 }
