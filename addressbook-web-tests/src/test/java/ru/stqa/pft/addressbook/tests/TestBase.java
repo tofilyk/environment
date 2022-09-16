d43493b7daa4c2da466618379de4bf25bec76e0a
Konstantin <tofilyk@mail.ru>
2022-09-16 22:07:18 +0300

    6.11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.AppclicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
 
     protected static final AppclicationManager app = new AppclicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -15,9 +24,21 @@ public class TestBase {
 
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+
+    }
+
+    @BeforeMethod
+    public void  logTestStart(Method m,Object[] p){
+        logger.info("Start test CreateGroupTest"+m.getName()+"with patametrs"+ Arrays.asList(p));
+
+
+    }
+    @AfterMethod(alwaysRun = true)
+    public  void logTestStop(Method m){
+        logger.info("Stop test CreateGroupTest"+m.getName());
 
     }
 
