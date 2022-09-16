7b36443fc2f291b200cb01a108ff673a405205f9
Konstantin <tofilyk@mail.ru>
2022-09-16 09:37:04 +0300

    6.4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTest.java
@@ -1,26 +1,41 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CreateGroupTest extends TestBase {
-    @Test//(enabled = false)
-    public void testCreateGroup() throws Exception {
+
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new GroupData().withName("name1").withFooter("footer1").withHeader("header1")});
+        list.add(new Object[]{new GroupData().withName("name2").withFooter("footer2").withHeader("header2")});
+        list.add(new Object[]{new GroupData().withName("name3").withFooter("footer3").withHeader("header3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testCreateGroup(GroupData group ) throws Exception {
+       // GroupData group = new GroupData().withName(name).withFooter(footer).withHeader(header);
         app.goTo().GroupPage();
         Groups before = app.Group().all();
-        GroupData group = new GroupData().withName("Group1");
         app.Group().create(group);
-        assertThat(app.Group().count(), equalTo(before.size()+1));
+        assertThat(app.Group().count(), equalTo(before.size() + 1));
         Groups after = app.Group().all();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
-    @Test//(enabled = false)
+    @Test(enabled = false)
     public void testCreateBadGroup() throws Exception {
         app.goTo().GroupPage();
         Groups before = app.Group().all();
