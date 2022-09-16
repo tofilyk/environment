9cc8a61299861ba01b3da9ceb77646b6ee5f7e6c
Konstantin <tofilyk@mail.ru>
2022-09-16 12:52:37 +0300

    6.6+6.7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,14 +1,22 @@
 package ru.stqa.pft.addressbook.model;
-
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
 import java.util.Objects;
 
+@XStreamAlias("group")
+
 public class GroupData {
+    @XStreamOmitField
 
-
-    private int id= Integer.MAX_VALUE;
+    private int id = Integer.MAX_VALUE;
+    @Expose
     private String name;
+    @Expose
     private String header;
+    @Expose
     private String footer;
+
 
     public int getId() {
         return id;
@@ -45,6 +53,7 @@ public class GroupData {
         this.footer = footer;
         return this;
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
