7dc73e44f114058ab3d4581f2b509ed41e094c4e
Konstantin <tofilyk@mail.ru>
2022-09-20 19:40:19 +0300

    logback

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,14 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.AppclicationManager;
-
-import java.lang.reflect.Method;
-import java.util.Arrays;
 
 public class TestBase {
     Logger logger = LoggerFactory.getLogger(TestBase.class);
@@ -30,16 +25,7 @@ public class TestBase {
 
     }
 
-    @BeforeMethod
-    public void  logTestStart(Method m,Object[] p){
-        logger.info("Start test CreateGroupTest"+m.getName()+"with patametrs"+ Arrays.asList(p));
-
-
-    }
-    @AfterMethod(alwaysRun = true)
-    public  void logTestStop(Method m){
-        logger.info("Stop test CreateGroupTest"+m.getName());
-
-    }
+   // @BeforeMethod   public void  logTestStart(Method m,Object[] p){       logger.info("Start test CreateGroupTest"+m.getName()+"with parameters"+ Arrays.asList(p));   }
+   // @AfterMethod(alwaysRun = true) public  void logTestStop(Method m){ logger.info("Stop test CreateGroupTest"+m.getName());   }
 
 }
