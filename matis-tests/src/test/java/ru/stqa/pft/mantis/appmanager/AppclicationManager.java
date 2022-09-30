135c587cc02abdf4a9c32b7466ce31b656d100e6
Konstantin <tofilyk@mail.ru>
2022-09-30 13:05:18 +0300

    8.2

---
+++ matis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/AppclicationManager.java
@@ -0,0 +1,60 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class AppclicationManager {
+
+    private final Properties properties;
+    public WebDriver wd;
+
+
+    private String browser;
+
+    public AppclicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+        wd.get(properties.getProperty("web.baseUrl"));
+
+    }
+
+    private void login(String username, String pass) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(pass);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+
+}
